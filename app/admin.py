ccbe525c3b929388a31f4b9dcc302f713fce0b5d
xiidot1303 <xii1303@inbox.ru>
2024-05-21 10:23:53 +0500

    add branch admin

--- app/admin.py
+++ app/admin.py
@@ -9,6 +9,10 @@ class Passport_dataAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
         return [field.name for field in self.model._meta.concrete_fields]
 
+class BranchAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Passport_data, Passport_dataAdmin)
+admin.site.register(Branch, BranchAdmin)
