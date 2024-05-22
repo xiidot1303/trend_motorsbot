e016aae2f5341584b52c0621793ae4270d36ab3c
xiidot1303 <xii1303@inbox.ru>
2024-05-22 15:26:04 +0500

    creating contact in amocrm service

--- app/services/amocrm_service.py
+++ app/services/amocrm_service.py
@@ -4,6 +4,12 @@ import aiofiles
 
 URL = "https://trendmotor.amocrm.ru"
 
+async def generate_headers():
+    async with aiofiles.open('amocrm_tokens/access.txt', mode='r') as f:
+        access_token = await f.read()
+    headers = {"Authorization": f"Bearer {access_token}"}
+    return headers
+
 async def update_tokens():
     async with aiofiles.open('amocrm_tokens/refresh.txt', mode='r') as f:
         refresh_token = await f.read()
@@ -29,12 +35,8 @@ async def update_tokens():
         await f.write(new_access_token)
 
 async def get_catalog():
-    # get access token
-    async with aiofiles.open('amocrm_tokens/access.txt', mode='r') as f:
-        access_token = await f.read()
-
     url = URL + '/api/v4/catalogs/12730/elements?limit=250&page=1'
-    headers = {"Authorization": f"Bearer {access_token}"}
+    headers = await generate_headers()
     result = []
     while True:
         content, h = await send_request(url, headers=headers)
@@ -49,11 +51,8 @@ async def get_catalog():
     return result
 
 async def get_vin_codes():
-    # get access token
-    async with aiofiles.open('amocrm_tokens/access.txt', mode='r') as f:
-        access_token = await f.read()
     url = URL + '/api/v4/catalogs/13091/elements?limit=250&page=1'
-    headers = {"Authorization": f"Bearer {access_token}"}
+    headers = generate_headers()
     result = []
     while True:
         content, h = await send_request(url, headers=headers)
@@ -67,3 +66,90 @@ async def get_vin_codes():
 
     return result
     
+async def create_contact(
+        name, surname, phone_number, passport_serial, passport_number,
+        date_birth, doc_give_place, date_begin_document, pnfl, birth_place
+    ):    
+    url = URL + '/api/v4/contacts'
+    headers = generate_headers()
+    method = "post"
+    data = [
+        {
+            "name": f"{name} {surname}",
+            "first_name": name,
+            "last_name": surname,
+            "responsible_user_id": 10287314,
+            "custom_fields_values": [
+                {
+                    "field_id": 1438451,
+                    "values": [
+                        {
+                            "value": phone_number,
+                            "enum_id": 858577,
+                            "enum_code": "WORK"
+                        }
+                    ]
+                },
+                {
+                    "field_id": 1561288,
+                    "values": [
+                        {
+                            "value": f"{passport_serial}{passport_number}"
+                        }
+                    ]
+                },
+                {
+                    "field_id": 1575435,
+                    "values": [
+                        {
+                            "value": date_birth
+                        }
+                    ]
+                },
+                {
+                    "field_id": 1597603,
+                    "values": [
+                        {
+                            "value": doc_give_place
+                        }
+                    ]
+                },
+                {
+                    "field_id": 1597609,
+                    "values": [
+                        {
+                            "value": date_begin_document
+                        }
+                    ]
+                },
+                {
+                    "field_id": 1598027,
+                    "values": [
+                        {
+                            "value": True
+                        }
+                    ]
+                },
+                {
+                    "field_id": 1451161,
+                    "values": [
+                        {
+                            "value": pnfl
+                        }
+                    ]
+                },
+                {
+                    "field_id": 1579197,
+                    "values": [
+                        {
+                            "value": birth_place
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+
+    content, h = await send_request(url, data, headers=headers, type=method)
+    contact_id = content['_embedded']['contacts'][0]['id']
+    return contact_id
