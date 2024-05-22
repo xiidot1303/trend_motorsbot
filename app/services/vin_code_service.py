5f33e2f9c41c58b0164791b55a7a441ac90d6801
xiidot1303 <xii1303@inbox.ru>
2024-05-22 12:11:03 +0500

    fix vincode service

--- app/services/vin_code_service.py
+++ app/services/vin_code_service.py
@@ -1,5 +1,6 @@
 from app.services import *
 from app.models import Vin_code, Product
+from asgiref.sync import sync_to_async
 
 async def update_or_create_vin_code(
         element_id, full_title, code, product_title, branch_title
@@ -12,12 +13,18 @@ async def update_or_create_vin_code(
             "branch_title": branch_title
         }
     )
-
+    obj_product = await get_object_product(obj)
     # set product if vin code is created
-    if is_created or obj.product == None:
+    if is_created or obj_product is None:
         # get product by title
-        product: Product = await Product.objects.aget(title=product_title)
-        obj.product = product
-        await obj.asave()
-        
+        try:
+            product: Product = await Product.objects.aget(title=product_title)
+            obj.product = product
+            await obj.asave()
+        except:
+            None
     return obj
+
+@sync_to_async
+def get_object_product(obj: Vin_code):
+    return obj.product
