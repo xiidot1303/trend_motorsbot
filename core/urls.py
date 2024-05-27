6b1ad7358c8372482f0f8b85ba037fd25e594bda
xiidot1303 <xii1303@inbox.ru>
2024-05-27 11:27:26 +0500

    change admin url

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('xiidot1303/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('', include('bot.urls')),
 ]
